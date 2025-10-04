75df8ee75213cd420d248f80c7861dd145384bcc
Le Thanh Lam <lamlethanh987@gmail.com>
2025-10-04 11:34:15 +0700

    feat: update VALORANT_MAPS to comment out unused maps and add new maps

--- ban-pick-tome/src/data/maps.ts
+++ ban-pick-tome/src/data/maps.ts
@@ -13,34 +13,58 @@ export const VALORANT_MAPS: ValorantMap[] = [
     image: '/maps_img/bind.avif',
     status: 'available'
   },
-  {
-    id: 'breeze',
-    name: 'Breeze',
-    image: '/maps_img/breeze.avif',
-    status: 'available'
-  },
+  // {
+  //   id: 'breeze',
+  //   name: 'Breeze',
+  //   image: '/maps_img/breeze.avif',
+  //   status: 'available'
+  // },
   {
     id: 'haven',
     name: 'Haven',
     image: '/maps_img/haven.avif',
     status: 'available'
   },
-  {
-    id: 'icebox',
-    name: 'Icebox',
-    image: '/maps_img/icebox.avif',
-    status: 'available'
-  },
+  // {
+  //   id: 'icebox',
+  //   name: 'Icebox',
+  //   image: '/maps_img/icebox.avif',
+  //   status: 'available'
+  // },
   {
     id: 'lotus',
     name: 'Lotus',
     image: '/maps_img/lotus.avif',
     status: 'available'
   },
+  // {
+  //   id: 'pearl',
+  //   name: 'Pearl',
+  //   image: '/maps_img/pearl.avif',
+  //   status: 'available'
+  // },
+  // {
+  //   id: 'fracture',
+  //   name: 'Fracture',
+  //   image: '/maps_img/fracture.avif',
+  //   status: 'available'
+  // },
   {
-    id: 'pearl',
-    name: 'Pearl',
-    image: '/maps_img/pearl.avif',
+    id: 'sunset',
+    name: 'Sunset',
+    image: '/maps_img/sunset.avif',
     status: 'available'
+  },
+  {
+    id: 'corrode',
+    name: 'Corrode',
+    image: '/maps_img/corrode.avif',
+    status: 'available'
+  },
+  {
+    id: "abyss",
+    name: "Abyss",
+    image: "/maps_img/abyss.avif",
+    status: "available"
   }
 ];
